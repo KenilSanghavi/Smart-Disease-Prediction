519adead19b867d8af2cb27949d15bb9a6d9cc3e
Kenil Sanghavi <kenilsanghavi2017@gmail.com>
2026-04-18 22:37:47 +0530

    Render deployment setup with PostgreSQL

--- smart_disease/settings.py
+++ smart_disease/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-local-dev-key-change-
 
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1,smart-disease-prediction-otlm.onrender.com').split(',')
 
 CSRF_TRUSTED_ORIGINS = [
     f"https://{host}" for host in ALLOWED_HOSTS if host not in ['localhost', '127.0.0.1']
